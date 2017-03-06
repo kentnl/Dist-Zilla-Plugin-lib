5ae6c5e1beaf1e54d0fff422b707fe9d2a200fe3
Kent Fredric <kentfredric@gmail.com>
2017-03-07 01:01:00 +1300

    Build results of 1a1be4a (on master)

--- lib/Dist/Zilla/Plugin/lib.pm
+++ lib/Dist/Zilla/Plugin/lib.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::lib;
 
-our $VERSION = '0.001001';
+our $VERSION = '0.001002';
 
 # ABSTRACT: A simpler bootstrap for a more civilised world
 
@@ -76,7 +76,7 @@ Dist::Zilla::Plugin::lib - A simpler bootstrap for a more civilised world
 
 =head1 VERSION
 
-version 0.001001
+version 0.001002
 
 =head1 SYNOPSIS
 
